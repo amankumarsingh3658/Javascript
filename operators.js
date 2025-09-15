20d659a1e8274e4082d89f78471ba50a2624720b
Aman Kumar Singh <amankumarsingh1969@gmail.com>
2025-09-15 21:00:08 +0530

    completed operators

---
+++ operators.js
@@ -0,0 +1,23 @@
+// Arithematic , comparision , logical , assignment , unary , ternary
+
+// + - * / % ** // Arithematic
+// == === != !==  < > <= >= // comparision
+// = // Assignment Operator
+// && || // logical
+// + - ++ -- typeof ! // Unary Operators
+// ?: // Ternary Operator
+
+//*------------------------------------------------------------------------------------------------------------*
+// Instance Of
+// Provides Instance of a value
+// Works only with reference values
+
+// let a = 12;
+// a instanceof Number; // false
+
+// let b = [];
+// b instanceof Array // true
+
+// let b = {}
+// b instanceof Object // true
+// b instanceof Array // false
