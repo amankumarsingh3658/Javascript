16640e5c2d3234e7aaa34173597920d584766bbe
Aman Kumar Singh <amankumarsingh1969@gmail.com>
2025-09-17 21:25:31 +0530

    completed loops

---
+++ loops.js
@@ -0,0 +1,53 @@
+// Loops
+// For, While , Do While
+// To Repeat Things
+
+// Keywords
+// break;
+// continue;
+
+// for-is vs for-of loop in JS
+
+//*------------------------------------------------------------------------------------------------------------*
+// for loop
+// Kaha se jaana hai
+// Kahan tak jaana hai
+// Kaise jaana hai
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+
+// }
+
+//*------------------------------------------------------------------------------------------------------------*
+// while loop
+// kaha se jaana hai
+// kab rukna hai
+// kaise jaana hai
+
+// while (condition) {
+
+// }
+
+//*------------------------------------------------------------------------------------------------------------*
+// do {
+
+// } while (condition);
+
+//*------------------------------------------------------------------------------------------------------------*
+// break and continue;
+
+// break:- returns out from the loop on a condition mostly
+// continue:- continue to next iteration of loop
+
+// for (let i = 0; i < 100; i++) {
+//     if(i === 32){
+//         break;
+//     }
+// } will return as the value becomes 32
+
+// for (let i = 0; i < array.length; i++) {
+//     if (i===32) {
+//         continue;
+//     }
+// } will skip 32
