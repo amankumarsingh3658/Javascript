2965de1d5df37aada837d00c2c806b72c72ad0c0
Aman Kumar Singh <amankumarsingh1969@gmail.com>
2025-09-23 21:29:56 +0530

    completed dom

---
+++ dom.js
@@ -0,0 +1,17 @@
+// let id = document.getElementById("abcd");
+// console.log(id);
+
+// let className = document.getElementsByClassName("abc");
+// console.log(className);
+
+// let abcd = document.querySelector("h1");
+// abcd.textContent = "Hello";
+
+// let img = document.querySelector("img");
+// img.setAttribute(
+//   "src",
+//   "https://avatars.githubusercontent.com/u/60209451?s=400&u=1e0f6feccedbc6a489f8e325422c6858606581a1&v=4"
+// );
+
+let heading = document.querySelector("#heading");
+heading.textContent = "Welcome to sheryians";
